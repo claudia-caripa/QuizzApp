3d09613ffa6cf293aebd55b63e2837a690a8e7e4
Claudia Sanchez <claudia.caripa@gmail.com>
2020-04-08 20:29:45 +0100

    Quiz App for Nanodegree Android Basics

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isCorrect1 = false;
+    boolean isCorrect2 = false;
+    boolean isCorrect3 = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    /**
+     * Handle the click event for the first radio button
+     */
+    public void firstOnRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.correct_answer_q1:
+                if (checked)
+                    isCorrect1 = true;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer1_q1:
+                if (checked)
+                    isCorrect1 = false;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer3_q1:
+                if (checked)
+                    isCorrect1 = false;
+                // Go to next question
+                break;
+        }
+
+
+    }
+
+    /**
+     * Handle the click event for the second radio button
+     */
+    public void secondOnRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.correct_answer_q2:
+                if (checked)
+                    isCorrect2 = true;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer1_q2:
+                if (checked)
+                    isCorrect2 = false;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer2_q2:
+                if (checked)
+                    isCorrect2 = false;
+                // Go to next question
+                break;
+        }
+
+    }
+
+    /**
+     * Handle the click event for the third radio button
+     */
+    public void thirdOnRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.correct_answer_q3:
+                if (checked)
+                    isCorrect3 = true;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer1_q3:
+                if (checked)
+                    isCorrect3 = false;
+                // Go to next question
+                break;
+        }
+
+        switch (view.getId()) {
+            case R.id.answer2_q3:
+                if (checked)
+                    isCorrect3 = false;
+                // Go to next question
+                break;
+        }
+
+
+    }
+
+
+    /**
+     * Check which answers are correct and add 1 point to the score for every correct
+     * answer
+     */
+
+    public void showScore(View view) {
+        int score = 0;
+
+        if (isCorrect1) {
+            score++;
+        }
+
+        if (isCorrect2) {
+            score++;
+        }
+
+        if (isCorrect3) {
+            score++;
+        }
+
+        CheckBox isSedimentary = (CheckBox) findViewById(R.id.answer1_q4);
+        boolean correct1_q4 = isSedimentary.isChecked();
+
+        CheckBox isIgneous = (CheckBox) findViewById(R.id.answer3_q4);
+        boolean correct2_q4 = isIgneous.isChecked();
+
+        CheckBox isMetamorphic = (CheckBox) findViewById(R.id.answer4_q4);
+        boolean correct3_q4 = isMetamorphic.isChecked();
+
+        if (correct1_q4) {
+            if (correct2_q4) {
+                if (correct3_q4) {
+                    score++;
+                }
+            }
+        }
+
+        EditText question5 = (EditText) findViewById(R.id.answer5_text_input);
+        String answerQuestion5 = question5.getText().toString();
+
+        if (answerQuestion5.equals("Crust")) {
+            score++;
+        }
+
+        //Get user name from the Edit Text and store it in a variable
+        EditText userName = (EditText) findViewById(R.id.name_text_input);
+        String name = userName.getText().toString();
+
+
+        //Display the final score using a Toast
+        Context context = getApplicationContext();
+        CharSequence text = "Hello " + name + " ,your score is: " + score;
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+}
